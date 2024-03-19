30db2c5f3e8a53111507f98d86a3a2f8c58b1de6
ShelsyDeRyck <114619980+ShelsyDeRyck@users.noreply.github.com>
2024-03-19 11:01:33 +0100

    added title left and added some style

--- adminpannel.php
+++ adminpannel.php
@@ -5,13 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin pannel</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
     <div class="container-fluid mt-5">
         <div class="row">
+            <div class="col-sm">
+            <a class="navbar-brand h1" href="#">Admin pannel</a>
+            <hr color="grey">
+            </div>
             <div class="col-md">
-            <h1>Admin pannel</h1>
-            <ul class="nav nav-tabs" id="myTab" role="tablist">
+            <ul class="nav nav-tabs justify-content-end" id="myTab" role="tablist">
                 <li class="nav-item" role="presentation">
                     <button class="nav-link active" id="login-tab" data-bs-toggle="tab" data-bs-target="#login-tab-pane" type="button" role="tab" aria-controls="login-tab-pane" aria-selected="true">Login</button>
                 </li>
