882290d0981cf94a566985b2dfb455a75090df99
ShelsyDeRyck <114619980+ShelsyDeRyck@users.noreply.github.com>
2024-03-20 10:42:04 +0100

    added bootstrap layout

--- login.php
+++ login.php
@@ -4,20 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
 <div class="container">
     <div class="login-form">
         <h2 class="text-center">Login</h2>
         <form action="" method="post">
-            <div class="form-group">
+            <div class="form-group mb-2">
                 <input type="text" name="username" class="form-control" placeholder="Username" required="required">
             </div>
-            <div class="form-group">
+            <div class="form-group mb-2">
                 <input type="password" name="password" class="form-control" placeholder="Password" required="required">
             </div>
             <div class="form-group">
-                <button type="submit" class="btn btn-primary btn-block">Log in</button>
+                <button type="submit" class="btn btn-danger btn-block">Log in</button>
             </div>
         </form>
     </div>
@@ -95,5 +97,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 // $conn->close();
 ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script> src='script.js'</script>
 </body>
 </html>
